bf3b617ed2d297f57c935946388f46108f016465
Anton Kibekin <kibekin1984anton@gmail.com>
2020-05-15 23:05:27 +0300

    Implementation method onCreate and sendMessage

--- app/src/main/java/com/kibekin/chatapp/MainActivity.java
+++ app/src/main/java/com/kibekin/chatapp/MainActivity.java
@@ -1,14 +1,54 @@
 package com.kibekin.chatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerViewMessages;
+    private MessagesAdapter adapter;
+
+    private EditText editTextMessage;
+    private ImageView imageViewSendMessage;
+
+    private List<Message> messages;
+    private String author;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerViewMessages = findViewById(R.id.recyclerViewMessages);
+        editTextMessage = findViewById(R.id.editTextMessage);
+        imageViewSendMessage = findViewById(R.id.imageViewSendMessage);
+        adapter = new MessagesAdapter();
+        recyclerViewMessages.setLayoutManager(new LinearLayoutManager(this));
+        recyclerViewMessages.setAdapter(adapter);
+        messages = new ArrayList<>();
+        author = "Андрей";
+        imageViewSendMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendMessage();
+            }
+        });
+    }
+
+    private void sendMessage(){
+        String textOfMessage = editTextMessage.getText().toString().trim();
+        if (!textOfMessage.isEmpty()) {
+            messages.add(new Message(author, textOfMessage));
+            adapter.setMessages(messages);
+            editTextMessage.setText("");
+        }
     }
 }
